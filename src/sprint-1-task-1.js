52b33352781b5410a15e0ad320bfc92a4227c434
ru39391 <dev@39391.ru>
2024-12-28 10:35:40 +0300

    заменить методы обработки массивов переборами

--- src/sprint-1-task-1.js
+++ src/sprint-1-task-1.js
@@ -1,89 +1,147 @@
 const handleData = (data) => data.toString().trim().split(' ').map(Number);
 
-const sliceArr = (arr, [start, end]) => typeof start === 'number' && typeof end === 'number' ? arr.slice(start, end) :  arr.slice(start);
+const sliceArr = (arr, [start, end], isReversed = false) => {
+    const array = [];
 
-const setValuesData = (array) => array.reduce(
-    (acc, value, index) => value === 0
-        ? {...acc, arr: [...acc.arr, {value, defaultIdx: index}], values: [...acc.values, index]}
-        : {...acc, arr: [...acc.arr, {value, defaultIdx: index}]},
-    {
-        arr: [],
-        values: []
-    }
-);
+    arr.forEach((item, index) => {
+        if(typeof start === 'number' && typeof end === 'number') {
+            if(index >= start && index < end) {
+                isReversed ? array.unshift(item) : array.push(item);
+            }
+        } else {
+            if(index >= start) {
+                isReversed ? array.unshift(item) : array.push(item);
+            }
+        }
+    });
 
-const handleValues = ({arr, values}) => arr.reduce(
-    (acc, { defaultIdx, value }) => value === 0 && values.indexOf(defaultIdx) !== values.length - 1
-        ? [...acc, sliceArr(arr, [defaultIdx + 1, values[values.indexOf(defaultIdx) + 1]])]
-        : acc, []
-);
+    return array;
+};
 
-const setIdx = (arr) => arr.map((data, index) => ({ ...data, idx: index + 1 }));
+const setValuesData = (array) => {
+    const arr = [];
+    const values = [];
+
+    array.forEach((value, index) => {
+        arr.push({value, defaultIdx: index});
+
+        if(value === 0) values.push(index);
+    });
+
+    return { arr, values };
+};
+
+const handleValues = ({arr, values}) => {
+    const array = [];
+
+    arr.forEach(({ defaultIdx, value }) => {
+        if(value === 0 && values.indexOf(defaultIdx) !== values.length - 1) {
+            array.push(
+                sliceArr(arr, [defaultIdx + 1, values[values.indexOf(defaultIdx) + 1]])
+            );
+        }
+    });
+
+    return array;
+};
+
+const setIdx = (arr) => {
+    const array = [];
+
+    arr.forEach((item, index) => array.push({ ...item, idx: index + 1 }));
+
+    return array;
+};
 
 const handleArray = (arr) => arr.length % 2 === 0
     ? [
-        ...setIdx([...arr].slice(0, [...arr].length / 2)),
-        ...setIdx([...arr].slice([...arr].length / 2).reverse())
-    ]
+        setIdx(sliceArr(arr, [0, arr.length / 2])),
+        setIdx(sliceArr(arr, [arr.length / 2], true))
+    ].flat()
     : [
-        ...setIdx([...arr].slice(0, ([...arr].length - 1) / 2)),
-        ...[{ ...[...arr][([...arr].length - 1) / 2], idx: ([...arr].length - 1) / 2 + 1 }],
-        ...setIdx([...arr].slice(([...arr].length - 1) / 2 + 1).reverse())
-    ];
+        setIdx(sliceArr(arr, [0, (arr.length - 1) / 2])),
+        [{ ...arr[(arr.length - 1) / 2], idx: (arr.length - 1) / 2 + 1 }],
+        setIdx(sliceArr(arr, [(arr.length - 1) / 2 + 1], true))
+    ].flat();
 
 const joinArray = (arr) => arr.join(' ')
 
 const handleDataValues = (data) => {
+    const valuesArr = [];
+    const intValuesArr = [];
+    const finalValues = [];
+    const startValues = [];
+    const endValues = [];
     const array = handleData(data);
-    const idxValues = array.map((_, index) => index);
-    const valuesArr = array.filter(value => value === 0);
-    const intValuesArr = array.filter(value => value > 0);
+    const startIdx = array.indexOf(0);
+    const endIdx = array.length - 1;
+
+    array.forEach(value => value > 0 ? intValuesArr.push(value) : valuesArr.push(value))
 
     if(valuesArr.length === 1) {
-        if(array.indexOf(0) === 0) {
-            return joinArray(idxValues);
+        if(startIdx === 0) {
+            array.forEach((_, index) => finalValues.push(index));
+
+            return joinArray(finalValues);
         }
 
-        if(array.indexOf(0) === array.length - 1) {
-            return joinArray(idxValues.reverse());
+        if(startIdx === endIdx) {
+            array.forEach((_, index) => finalValues.unshift(index));
+
+            return joinArray(finalValues);
         }
 
-        if(array.indexOf(0) > 0 && array.indexOf(0) < array[array.length - 1]) {
-            return joinArray([
-                ...[...array].slice(0, array.indexOf(0)).map((_, index) => index + 1).reverse(),
-                0,
-                ...[...array].slice(array.indexOf(0) + 1).map((_, index) => index + 1)
-            ]);
+        if(startIdx > 0 && startIdx < array[endIdx]) {
+            array.forEach((_, index) => {
+                if(index < startIdx) finalValues.unshift(index + 1);
+            });
+
+            finalValues.push(0);
+
+            array.forEach((_, index) => {
+                if(index >= startIdx + 1) finalValues.push(index - startIdx);
+            });
+
+            return joinArray(finalValues);
         }
     }
 
     if(intValuesArr.length === 1) {
-        return joinArray(array.map(value => Boolean(value) ? 1 : 0));
+        array.forEach(value => finalValues.push(Boolean(value) ? 1 : 0));
+
+        return joinArray(finalValues);
     }
 
     if(valuesArr.length === array.length) {
-        return joinArray(array.map((_) => 0));
+        array.forEach(() => finalValues.push(0));
+
+        return joinArray(finalValues);
     }
 
     const {arr, values} = setValuesData(array);
-    const leftValues = arr.reduce((acc, item, index) => index < values[0] ? [...acc, item] : acc, []);
-    const rightValues = arr.reduce((acc, item, index) => index > values[values.length - 1] ? [...acc, item] : acc, []);
+
+    arr.forEach((item, index) => {
+        if(index < values[0]) startValues.unshift(item);
+    });
+
+    arr.forEach((item, index) => {
+        if(index > values[values.length - 1]) endValues.push(item);
+    });
+
     const handledValues = [
-        ...setIdx(leftValues.reverse()),
-        ...handleValues({arr, values})
-            .map(
-                (item) => item.length === 1 ? setIdx(item) : handleArray(item)
-            ).reduce(
-                (acc, item) => [...acc, ...item], []
-            ),
-        ...setIdx(rightValues)
-    ];
+        setIdx(startValues),
+        handleValues({arr, values}).map((item) => item.length === 1 ? setIdx(item) : handleArray(item)).flat(),
+        setIdx(endValues)
+    ].flat();
 
-    return joinArray(
-        arr.map((item, index) => item.value > 0 ? handledValues.find(({ defaultIdx, value }) => defaultIdx === index && value === item.value).idx : 0)
-    );
+    arr.forEach((item, index) => {
+        finalValues.push(
+            item.value > 0 ? handledValues.flat().find(({ defaultIdx, value }) => defaultIdx === index && value === item.value).idx : 0
+        );
+    });
+
+    return joinArray(finalValues);
 };
-
 
 export default handleDataValues;
 
